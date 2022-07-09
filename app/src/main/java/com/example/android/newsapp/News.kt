5f9dd8965a5a97b5bbacb56b5a07d36d46b9c9c1
manthan <manthanmoharana03583@gmail.com>
2022-07-09 20:41:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/newsapp/News.kt
@@ -0,0 +1,9 @@
+package com.example.android.newsapp
+
+data class News (
+    val title: String,
+    val author: String,
+    val url: String,
+    val imageUrl: String,
+    val description: String,
+    val content: String)
